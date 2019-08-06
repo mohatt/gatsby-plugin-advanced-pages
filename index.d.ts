a2cb274830943a4173d13ccef558258d5ba53e5b
Mohamed Elkholy <mkh117@gmail.com>
2019-08-06 16:39:44 +0200

    Added type definitions for Typescript and Flow

---
+++ index.d.ts
@@ -0,0 +1,54 @@
+import * as React from 'react';
+
+export interface Route {
+  path: string;
+  scopes: {
+    [index: string]: string;
+  }
+}
+
+export interface RouteParams {
+  [index: string]: string;
+}
+
+export interface PaginationInfo {
+  itemCount: number;
+  currentPage: number;
+  perPage: number;
+}
+
+export interface PaginationProps {
+  route: string;
+  params?: RouteParams;
+  ui?: string;
+  range?: number;
+  className?: string;
+  renderDisabled?: boolean;
+  pageInfo: PaginationInfo;
+  labels?: {
+    [index: string]: string | React.ReactNode
+  };
+  theme?: {
+    [index: string]: string
+  };
+}
+
+export interface LinkProps {
+  to: string;
+  params?: RouteParams;
+  scope?: string
+}
+
+export const Pagination: React.ComponentType<PaginationProps>;
+export const Link: React.ComponentType<LinkProps>;
+
+export function getRoute(routeName: string): Route;
+export function getPathGenerator(
+  routeName: number,
+  scope?: string
+): (args?: RouteParams) => string;
+export function generatePath(
+  routeName: number,
+  args?: RouteParams,
+  scope?: string
+): string;
