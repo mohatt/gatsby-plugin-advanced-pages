1470e6e7550a359ddbd420daf2fdc918b0572152
Mohamed Elkholy <mkh117@gmail.com>
2019-08-09 15:57:17 +0200

    Don't transpile graphql fragment file, leave it to gatsby

--- src/gatsby/install-fragments.js
+++ src/gatsby/install-fragments.js
@@ -5,7 +5,7 @@ export default async function ({ store, getNodesByType }) {
   const { program } = store.getState()
   // Add pagination fragment to .cache/fragments.
   await fs.copyFile(
-    path.resolve(__dirname, '../components/Pagination/Pagination.fragment.js'),
+    path.resolve(__dirname, '../components/Pagination/Pagination.fragment'),
     path.resolve(program.directory, '.cache/fragments/pagination-fragment.js'),
     err => {
       if (err) throw err;
