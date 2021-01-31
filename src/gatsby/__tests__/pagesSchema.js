e0c0af28a77148f33511298ee65e4c23e2400ea9
Mohamed Elkholy <mkh117@gmail.com>
2021-02-01 01:48:23 +0200

    test(plugin): added tests for "pagesSchema"

---
+++ src/gatsby/__tests__/pagesSchema.js
@@ -0,0 +1,16 @@
+import { testPluginOptionsSchema } from "gatsby-plugin-utils"
+import { pagesSchema } from '../schema'
+import testCases from '../../../test/__fixtures__/pagesSchema'
+
+describe('pluginOptionsSchema', () => {
+  for (const { title, pages } of testCases) {
+    it(title, async () => {
+      const { isValid, errors } = await testPluginOptionsSchema(
+        ({ Joi }) => pagesSchema(Joi), pages
+      )
+
+      expect(isValid).toMatchSnapshot()
+      expect(errors).toMatchSnapshot()
+    })
+  }
+})
